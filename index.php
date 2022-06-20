af6e96e258d493c6054f634aafd397187e57d5b6
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2022-06-20 15:55:01 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  
+?>
+<!DOCTYPE html><html>
+<head>
+    <title>PODIUM</title>
+    <link rel="stylesheet" href="dbase.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <?php
+		include 'include/nav_con.php';
+    ?>
+
+    
+    </html>
