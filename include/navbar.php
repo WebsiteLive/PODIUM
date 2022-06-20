32d8402334d416a75e1483f77c83d9e1bc07fcbd
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2022-06-20 17:04:11 +0800

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -1,8 +1,10 @@
-<ul>
-    <li>
-        <a>Home</a>
-    </li>
-    <li>
-        <a>Home</a>
-    </li>
-</ul>
+<nav  class="navbar navbar-toggleable-md navbar-dark bg-primary">
+    <ul class=navbar-nav>
+        <li class=navbar-nav>
+            <a  class=nav-item>Home</a>
+        </li>
+        <li class=navbar-nav>
+            <a  class=nav-item>Home</a>
+        </li>
+    </ul>
+</nav>
