0fd2a4bff74ca8950a6bacde0415d793746b13a0
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2022-06-20 17:04:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 <!DOCTYPE html><html>
 <head>
     <title>PODIUM</title>
-    <link rel="stylesheet" href="dbase.css">
+    <link rel="stylesheet" href=".css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 </head>
 <body>
     <?php
-		include 'include/nav_con.php';
+		include 'include/navbar.php';
     ?>
 
-    
-    </html>
+</html>
