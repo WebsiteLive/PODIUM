fe2038bea5058e241e99193bddd71599bc80e949
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2022-06-22 17:04:43 +0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,43 @@
+<?php
+  
+?>
+<!DOCTYPE html><html>
+<head>
+    <title>PODIUM</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+</head>
+<body class="body">
+<div class="container ">
+  <div class="row justify-content-center">
+    <div class="col-sm-4 signup_form">
+      <h2>Sign Up</h2>
+      <form>
+      <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="form_input" ariadescribedby="emailHelp" placeholder="Enter email">
+        </div> 
+        <div class="form-group">
+          <label for="exampleInputEmail1">Name:</label>
+          <input type="email" class="form-control" id="form_input" ariadescribedby="emailHelp">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">User Name:</label>
+          <input type="email" class="form-control" id="form_input" ariadescribedby="emailHelp">
+        </div>  
+        <div class="form-group">
+          <label for="exampleInputEmail1">Password:</label>
+          <input type="email" class="form-control" id="form_input" ariadescribedby="emailHelp">
+        </div> 
+        <div class="form-group">
+          <label for="exampleInputEmail1">Confirm Password:</label>
+          <input type="email" class="form-control" id="form_input" ariadescribedby="emailHelp">
+        </div> 
+
+        <button type="submit" class="signup_btn align-self-center">Sign Up</button>
+        
+      </form>
+    </div>
+  </div>
+</div>
+</html>
