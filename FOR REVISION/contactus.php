d769be5e9310efdf9f819effbef5d27d658fa09d
jtballesteros <104001909+jtballesteros@users.noreply.github.com>
2022-06-27 14:35:48 +0800

    Contact Us and image folder

---
+++ FOR REVISION/contactus.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="contactus.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <br><br>
+            <h3><span>CONTACT US</span></h3>
+            <h4><span>Feel free to leave a message</span></h4>
+        </div>
+        <div class="contact">
+            <form action="#" method="post">
+                <label for="name">Name : </label> 
+                <input type="text" name="name" placeholder="  Enter Name" size="50" required=""> <br><br>
+                <label for="email">Email : </label> 
+                <input type="email" name="email" placeholder="  Enter Email Address" size="50" required=""> <br><br>
+                <label for="">Message :</label>
+                <textarea name="form-control" id="" cols="50" rows="7" placeholder="Enter Message"></textarea>
+                
+                <br><br>
+                <button type="submit">SUBMIT</button>
+                <br><br>
+            </form>
+        </div>
+
+    </div>
+</body>
+</html>
