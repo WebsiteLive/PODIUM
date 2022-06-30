53dcf679728b2275b9c8c983c6f8d08439fda724
Camy0801 <franzgudio@gmail.com>
2022-06-30 17:25:19 +0800

    changes

--- freelancerpage.php
+++ freelancerpage.php
@@ -88,48 +88,209 @@
                           </div>
 
 
-             <div class="gallery">
+                          <div class="container">
+                
+                <h3 class="title"> organic products </h3>
+             
+                <div class="products-container">
+             
+                   <div class="product" data-name="p-1">
+                      <img src="img/art1.jpg" alt="">
+                      <h3>strawberries</h3>
+                      <div class="price">$2.00</div>
+                   </div>
+             
+                   <div class="product" data-name="p-2">
+                      <img src="img/art1.jpg" alt="">
+                      <h3>onions</h3>
+                      <div class="price">$2.00</div>
+                   </div>
+             
+                   <div class="product" data-name="p-3">
+                      <img src="img/art1.jpg" alt="">
+                      <h3>tomatoes</h3>
+                      <div class="price">$2.00</div>
+                   </div>
+             
+                   <div class="product" data-name="p-4">
+                      <img src="img/art1.jpg" alt="">
+                      <h3>brinjal</h3>
+                      <div class="price">$2.00</div>
+                   </div>
+             
+                   <div class="product" data-name="p-5">
+                      <img src="img/art1.jpg" alt="">
+                      <h3>broccoli</h3>
+                      <div class="price">$2.00</div>
+                   </div>
+             
+                   <div class="product" data-name="p-6">
+                      <img src="img/art1.jpg" alt="">
+                      <h3>potatoes</h3>
+                      <div class="price">$2.00</div>
+                   </div>
+             
+                </div>
+             
+             </div>
+             
+             <div class="products-preview">
+             
+                <div class="preview" data-target="p-1">
+                   <i class="fas fa-times"></i>
+                   <img src="img/art1.jpg" alt="">
+                   <h3>organic strawberries</h3>
+                   <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half-alt"></i>
+                      <span>( 250 )</span>
+                   </div>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+                   <div class="price">$2.00</div>
+                   <div class="buttons">
+                      <a href="#" class="buy">buy now</a>
+                      <a href="#" class="cart">add to cart</a>
+                   </div>
+                </div>
+             
+                <div class="preview" data-target="p-2">
+                   <i class="fas fa-times"></i>
+                   <img src="img/art1.jpg" alt="">
+                   <h3>organic onions</h3>
+                   <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half-alt"></i>
+                      <span>( 250 )</span>
+                   </div>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+                   <div class="price">$2.00</div>
+                   <div class="buttons">
+                      <a href="#" class="buy">buy now</a>
+                      <a href="#" class="cart">add to cart</a>
+                   </div>
+                </div>
+             
+                <div class="preview" data-target="p-3">
+                   <i class="fas fa-times"></i>
+                   <img src="img/art1.jpg" alt="">
+                   <h3>organic tomatoes</h3>
+                   <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half-alt"></i>
+                      <span>( 250 )</span>
+                   </div>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+                   <div class="price">$2.00</div>
+                   <div class="buttons">
+                      <a href="#" class="buy">buy now</a>
+                      <a href="#" class="cart">add to cart</a>
+                   </div>
+                </div>
+             
+                <div class="preview" data-target="p-4">
+                   <i class="fas fa-times"></i>
+                   <img src="img/art1.jpg" alt="">
+                   <h3>organic brinjal</h3>
+                   <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half-alt"></i>
+                      <span>( 250 )</span>
+                   </div>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+                   <div class="price">$2.00</div>
+                   <div class="buttons">
+                      <a href="#" class="buy">buy now</a>
+                      <a href="#" class="cart">add to cart</a>
+                   </div>
+                </div>
+             
+                <div class="preview" data-target="p-5">
+                   <i class="fas fa-times"></i>
+                   <img src="img/art1.jpg" alt="">
+                   <h3>organic broccoli</h3>
+                   <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half-alt"></i>
+                      <span>( 250 )</span>
+                   </div>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+                   <div class="price">$2.00</div>
+                   <div class="buttons">
+                      <a href="#" class="buy">buy now</a>
+                      <a href="#" class="cart">add to cart</a>
+                   </div>
+                </div>
+             
+                <div class="preview" data-target="p-6">
+                   <i class="fas fa-times"></i>
+                   <img src="img/art1.jpg" alt="">
+                   <h3>organic potatoes</h3>
+                   <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half-alt"></i>
+                      <span>( 250 )</span>
+                   </div>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
+                   <div class="price">$2.00</div>
+                   <div class="buttons">
+                      <a href="#" class="buy">buy now</a>
+                      <a href="#" class="cart">add to cart</a>
+                   </div>
+                </div>
+             
+             </div>
+             
+             </body>
+             </html>
+                       <!----->
+                       
 
-             <div class="col-lg-4 col-sm-6">
-                    <div class="product-cate" data-name="painting">
-                      <div>
-                   <img src="img/drawing3.jpg"alt="">
-                   <div class="product-icon">
-                      <ul>
-                    <li><a href=""><i class="fas fa-heart"></i></a> </li>
-                    
-                    
-                      </ul>
-                      </div>
-                      <!------>
-                      <div class="product-des"data-name="painting">
-                        <li><a href=""><i class="fa-solid fa-user-large"></i></a> Camille Gudio</li>
-                        <br>
-                          <p> Open for Drawing Commission</p>
-                          
-                         
-                          <br>
-                          
 
-                      </div>
-                      </div>
-                              </div>
-                          </div>
-                          <!----->
-                          
-                                                      <div class="preview-box">
-                                                        <div class="details">
-                                                          <span class="title">Image Category: <p></p></span>
-                                                          <span class="icon fas fa-times"></span>
-                                                        </div>
-                                                        <div class="image-box"><img src="" alt=""></div>
-                                                      </div>
-                                                      <div class="shadow"></div>
-                                                      <script src="gallerymarketpage.js"></script>
-                                                    
-                                              <!----->
-                     
-      <!--End of Sidebar-->
+
+
+
+<script type="text/javascript">
+ let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product =>{
+product.onclick = () =>{
+ preveiwContainer.style.display = 'flex';
+ let name = product.getAttribute('data-name');
+ previewBox.forEach(preview =>{
+   let target = preview.getAttribute('data-target');
+   if(name == target){
+     preview.classList.add('active');
+   }
+ });
+};
+});
+
+previewBox.forEach(close =>{
+close.querySelector('.fa-times').onclick = () =>{
+ close.classList.remove('active');
+ preveiwContainer.style.display = 'none';
+};
+});
+</script>
 
 
 
@@ -141,24 +302,11 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+   <script src="gallerymarketpage.js"></script>
+ <!-- Optional JavaScript -->
+ <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
