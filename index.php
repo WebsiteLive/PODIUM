df64c43e92ee07d17f1b0cdf807472af954949f2
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2022-07-03 11:47:03 +0800

    change

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <title>PODIUM</title>
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="styles/descriptionOfArt.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
 </head>
 <body>
 
